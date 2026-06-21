7a25a4b09fe7db557f772a2973187a9b6e44f498
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-06-22 08:38:37 +0900

    260622 06_Object
    - 전체 소스 코드 재 업로드

--- 06_Object/src/com/kh/basic/Person.java
+++ 06_Object/src/com/kh/basic/Person.java
@@ -50,6 +50,36 @@ public class Person {
 		this.gender = gender;
 	}
 	// --------------------------------------
+	
+	// * ---------- 메소드부 ------------------
+	/**
+	 * 소개하는 메소드
+	 * : 저장된 이름, 나이, 성별 정보를 출력
+	 */
+	// 접근제한자 예약어 반환형 메소드명(매개변수) {
+	//     실행할 내용
+	// }
+	public void introduce() {
+		System.out.println(this.name + ", " + this.age + ", " + this.gender);
+	}
+	
+	/**
+	 * 말하다
+	 * : "안녕하세요" 출력
+	 */
+	public void talk() {
+		System.out.println("안녕하세요.");
+	}
+	
+	/**
+	 * 울다
+	 * : "엉엉" 출력
+	 */
+	public void cry() {
+		System.out.println("엉엉");
+	}
+	
+	// --------------------------------------	
 }
 
 
--- 06_Object/src/com/kh/method/Method2.java
+++ 06_Object/src/com/kh/method/Method2.java
@@ -27,7 +27,7 @@ public class Method2 {
 	public static int add(int n1, int n2) {
 		return n1 + n2;
 	}
-	
+//	
 //	public static double add(int n1, int n2) {
 //		
 //	}
